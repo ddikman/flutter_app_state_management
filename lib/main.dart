2f53be1e2c2f8e0b23a8102debc4bf80aa90254c
David Dikman <ddikman@gmail.com>
2018-06-10 11:25:03 +0800

    Updated demo title to better reflect contents.

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,12 @@ class AppRoot extends StatelessWidget {
   Widget build(BuildContext context) {
     // initialise app and state
     return new MaterialApp(
-        title: 'Inherited state demo',
+        title: 'Propagated state demo',
         theme: new ThemeData(
             primarySwatch: primaryColor,
             // the hintColor is used for the TextField input border color
             hintColor: fontColor,
             textTheme: Theme.of(context).textTheme.apply(bodyColor: fontColor)),
-        home: new MainScreen(title: 'Inherited state demo'));
+        home: new MainScreen(title: 'Propagated state demo'));
   }
 }
